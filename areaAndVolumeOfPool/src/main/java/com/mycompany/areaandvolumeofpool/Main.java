7cfb6d37467228bf1152e85817eb1c3a2141e600
aaronc991 <aaroncunningham222@gmail.com>
2022-02-08 21:35:15 +0000

    update

--- areaAndVolumeOfPool/src/main/java/com/mycompany/areaandvolumeofpool/Main.java
+++ areaAndVolumeOfPool/src/main/java/com/mycompany/areaandvolumeofpool/Main.java
@@ -3,7 +3,6 @@ package com.mycompany.areaandvolumeofpool;
 
 import java.util.Scanner;
 
-
 public class Main {
 
     private static final double PI = 3.14;
@@ -14,9 +13,9 @@ public class Main {
 
     public static void main(String[] args) {
         //call the menu method
-        //menu();
-        rectangleVolume();
-        cylinderInternalSurfaceArea();
+        menu();
+        //rectangleVolume();
+        //cylinderInternalSurfaceArea();
         //System.out.println("m\u00B3");
     }
 
@@ -39,6 +38,7 @@ public class Main {
             switch (option) {
                 case 2:
                     rectangleVolume();
+                    rectangleInternalSurfaceArea();
                     break;
                 case 3:
                     cylinderInternalSurfaceArea();
@@ -54,54 +54,52 @@ public class Main {
             }//end of switch
 
         }//end of while loop
-
+        
         menu();//puts the method in a loop even after selection until exit
-
+            
     }
- 
+
     private static void getName() {
         System.out.println("Please input your name");
         name = sc.nextLine();
     }
 
-    private static void rectangleInternalSurfaceArea() {
-        // = 2x (2x4 + 2x2 + 4x2) = 40m^2
-        length = sc.nextInt();
-        width = sc.nextInt();
-        shallowDepth = sc.nextInt();
+    private static void rectangleVolume() {
+        System.out.println("Input shallow depth of the pool");
+        shallowDepth = sc.nextDouble();
+        System.out.println("Input deep depth of the pool");
+        deepDepth = sc.nextDouble();
+        System.out.println("Input the width of the pool");
+        width = sc.nextDouble();
+        System.out.println("Input the length of the pool");
+        length = sc.nextDouble();
 
-        internalSurfaceArea = (2 * (length * width) + (width * shallowDepth));
+        poolVolume = (deepDepth + shallowDepth) * width * length / 2;
+
+        System.out.println(name + " The measurements you have entered for the pool are " + width + "m for the width " + length + "m for the length " + shallowDepth + "m for the shallow depth " + deepDepth + "m for the deep depth");
+        System.out.println("The volume of the pool is " + poolVolume + "m\u00B3");
 
     }
 
-    private static void rectangleVolume() {
-        System.out.println("Input shallow depth of the pool");
-        shallowDepth = 3;
-        System.out.println("Input deep depth of the pool");
-        deepDepth = 1;
-        System.out.println("Input the width of the pool");
-        width = 11;
-        System.out.println("Input the length of the pool");
-        length = 11;
-        
-        poolVolume = (deepDepth + shallowDepth) * width * length / 2;
-        
-        
-        System.out.println("The volume of the pool is " + poolVolume + "m\u00B3");
-        
-        
-        
+    private static void rectangleInternalSurfaceArea() {
+        //Rectangular or Freeform Shapes: L X W X 1.6 = Square Meters of Interior Surface
+        // = 2x (2x4 + 2x2 + 4x2) = 40m^2
+        //internalSurfaceArea = (2 * (length * width) + (width * shallowDepth));
+
+        internalSurfaceArea = length * width * 1.6;
+        System.out.println("The internal surface area is " + internalSurfaceArea + "m\u00B2");
+
     }
 
     private static void cylinderInternalSurfaceArea() {
-        
+
     }
 
     private static void cylinderVolume() {
 
     }
 
-    private static void outputMethod() {
+    private static void outputMethodRectangle() {
 
     }
 }
